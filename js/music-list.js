c0630d7855342f7ff2d64e08560d374cd4fdfdbd
Aman Verma <8211amanverma@gmail.com>
2022-03-16 02:12:57 +0530

    added

---
+++ js/music-list.js
@@ -0,0 +1,92 @@
+let allMusic = [
+    {
+      name: "Jalebi Baby",
+      artist: "Tesher x Jason Derulo",
+      img: "music-2",
+      src: "music-4"
+    },
+    {
+      name: "Excuses",
+      artist: "AP_Dhillion",
+      img: "music-1",
+      src: "music-3"
+    },
+    {
+      name: "Meri Maa Ke Barabar Koi Nahi",
+      artist: "Jubin Nautiyal",
+      img: "music-6",
+      src: "music-5"
+    },
+    {
+      name: "Memories",
+      artist: "Maroon - 5",
+      img: "music-4",
+      src: "music-6"
+    },
+    {
+      name: "Love Nwantiti Remix",
+      artist: "Joeboy & kuami Eungene",
+      img: "music-2",
+      src: "music-1"
+    },
+    {
+      name: "Girls Like You",
+      artist: "Beauz & ft.Cardi",
+      img: "music-3",
+      src: "music-2"
+    },
+    {
+      name: "Savage Love",
+      artist: "Jason Derulo",
+      img: "music-4",
+      src: "music-9"
+    },
+    {
+      name: "Raabta",
+      artist: "Arijit Singh",
+      img: "music-5",
+      src: "music-7"
+    },
+    {
+      name: "Desi Crew-Impress2",
+      artist: "Bunty Bains",
+      img: "music-6",
+      src: "music-8"
+    },
+    {
+      name: "Closer - The Chainsmokers",
+      artist: "NCS Release",
+      img: "music-2",
+      src: "music-10"
+    },
+    {
+      name: "Chaand Balliyan Reverbed",
+      artist: "NCS Release",
+      img: "music-1",
+      src: "music-13"
+    },
+    {
+      name: "Wallian",
+      artist: "Harnoor Gifty Rubbal",
+      img: "music-3",
+      src: "music-15"
+    },
+    {
+      name: "The PropheC-Kina Chir",
+      artist: "NCS Release",
+      img: "music-6",
+      src: "music-14"
+    },
+    {
+      name: "Hawa Banke",
+      artist: "Darshan Rawal",
+      img: "music-4",
+      src: "music-12"
+    },
+    {
+      name: "Señorita.mp3",
+      artist: "Shawn Mendes, Camila Cabello",
+      img: "music-1",
+      src: "music-11"
+    },
+  ];
